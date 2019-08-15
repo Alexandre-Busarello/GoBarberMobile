236db4cfff4abd63db10a8d4c205cfe249a43f36
Alexandre Busarello <busamar@LAPTOP-NKAJQ9SE.localdomain>
2019-08-15 22:02:34 +0000

    First commit GoBarber

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { withNavigationFocus } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
+
+function Dashboard({ isFocused }) {
+  const [appointments, setAppointments] = useState([]);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments?page=1');
+      setAppointments(response.data);
+    }
+
+    if (isFocused) {
+      loadAppointments();
+    }
+  }, [isFocused]);
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamento',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
+
+export default withNavigationFocus(Dashboard);
---
+++ src/pages/New/Confirm/index.js
@@ -0,0 +1,64 @@
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatRelative, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Background from '~/components/Background';
+
+import api from '~/services/api';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const timeFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: ptBR }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url.replace('localhost', '10.0.3.2')
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{timeFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
+}
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
